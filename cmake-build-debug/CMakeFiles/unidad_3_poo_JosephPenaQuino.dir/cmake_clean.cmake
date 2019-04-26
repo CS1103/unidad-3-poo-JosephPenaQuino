ea0402d06d5f92c86caf3e0d71620c68194f44dd
JosephPenaQuino <joseph.pena@utec.edu.pe>
2019-04-26 18:40:53 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/unidad_3_poo_JosephPenaQuino.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unidad_3_poo_JosephPenaQuino.dir/main.cpp.o"
+  "unidad_3_poo_JosephPenaQuino.pdb"
+  "unidad_3_poo_JosephPenaQuino"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unidad_3_poo_JosephPenaQuino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/unidad_3_poo_JosephPenaQuino.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for unidad_3_poo_JosephPenaQuino.
+# This may be replaced when dependencies are built.
